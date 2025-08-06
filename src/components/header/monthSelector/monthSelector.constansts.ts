bb2e1745b44fcefa9a69f807cf25a1cf0a0ad3f6
Aslly Zuñiga <asllyz2610@gmail.com>
2025-08-06 14:59:15 -0500

    Create monthSelector constants

---
+++ src/components/header/monthSelector/monthSelector.constansts.ts
@@ -0,0 +1,14 @@
+export const MONTHS_OPTIONS = [
+  { label: "Ene", value: "01" },
+  { label: "Feb", value: "02" },
+  { label: "Mar", value: "03" },
+  { label: "Abr", value: "04" },
+  { label: "May", value: "05" },
+  { label: "Jun", value: "06" },
+  { label: "Jul", value: "07" },
+  { label: "Ago", value: "08" },
+  { label: "Sep", value: "09" },
+  { label: "Oct", value: "10" },
+  { label: "Nov", value: "11" },
+  { label: "Dic", value: "12" },
+];
