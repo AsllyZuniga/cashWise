66acdff23a741b8c0a43077e23920fd5387c06ea
Aslly Zuñiga <asllyz2610@gmail.com>
2025-08-06 14:44:49 -0500

    Refactor: moved layout to Dashboard page and cleaned up App.tsx

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/header/header";
+import MonthSelector from "../components/header/monthSelector/monthSelector";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      <MonthSelector />
+      <main></main>
+    </>
+  );
+};
+
+export default Dashboard;
