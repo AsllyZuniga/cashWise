3052c9f6f37140dd4d4f7856019de9db67f1946f
Aslly Zuñiga <asllyz2610@gmail.com>
2025-08-06 14:54:54 -0500

    reate Header component with year selector and info cards

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,16 +5,18 @@ import { YEARS_OPTIONS } from "./header.constants";
 
 export default function Header() {
   return (
-    <header className="header">
-      <div className="header__container">
-        <div className="header__top">
-          <h1 className="header__title">Mis finanzas personales</h1>
-          <UiSelect className="header__select" options={YEARS_OPTIONS}/>
+    <>
+      <header className="header">
+        <div className="header__container">
+          <div className="header__top">
+            <h1 className="header__title">Mis finanzas personales</h1>
+            <UiSelect className="header__select" options={YEARS_OPTIONS} />
+          </div>
+          <div className="header__cards">
+            <HeaderCards />
+          </div>
         </div>
-        <div className="header__cards">
-          <HeaderCards />
-        </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
