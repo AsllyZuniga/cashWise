dbed2b6755c2df62cdb89b1249047e060fb8432e
Aslly Zuñiga <asllyz2610@gmail.com>
2025-08-06 15:05:08 -0500

    feat: add month selection logic and active state

---
+++ src/components/header/monthSelector/monthSelector.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./monthselector.scss";
+import { MONTHS_OPTIONS } from "./monthSelector.constansts";
+import { UiButton } from "../../../webComponents";
+
+export default function MonthSelector() {
+  const [selectedMonth, setSelectedMonth] = useState("");
+
+  return (
+    <div className="month-selector">
+      {MONTHS_OPTIONS.map((month) => (
+        <UiButton
+          key={month.value}
+          className={`month-selector__button ${
+            selectedMonth === month.value ? "active" : ""
+          }`}
+          onClick={() => setSelectedMonth(month.value)}
+        >
+          {month.label}
+        </UiButton>
+      ))}
+    </div>
+  );
+}
