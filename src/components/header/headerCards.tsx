292379321bc9e304fd4486db92a695816357fb10
Aslly Zuñiga <asllyz2610@gmail.com>
2025-08-18 22:55:21 -0500

    integrate BudgetModalForm into header cards

--- src/components/header/headerCards.tsx
+++ src/components/header/headerCards.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { HEADER_CARDS } from "./header.constants";
 import { UiCard, UiButton } from "../../webComponents";
+import BudgetModalForm from "../budgetModalForm/budgetModalForm";
 
 export default function HeaderCards() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <>
       {HEADER_CARDS.map((card, index) => (
@@ -9,14 +13,24 @@ export default function HeaderCards() {
           <div className="card-content">
             <img src={card.image} alt={card.title} className="card-image" />
             <div className="card-content-elements">
-              <h3>{card.title}: {card.value}</h3>
+              <h3>
+                {card.title}: {card.value}
+              </h3>
               {index === 0 && (
-                <UiButton className="btn-agregar">Agregar</UiButton>
+                <UiButton
+                  className="btn-agregar"
+                  onClick={() => setOpenModal(true)}
+                >
+                  Agregar
+                </UiButton>
               )}
             </div>
           </div>
         </UiCard>
       ))}
+
+      {/* Modal */}
+      <BudgetModalForm isOpen={openModal} onClose={() => setOpenModal(false)} />
     </>
   );
 }
