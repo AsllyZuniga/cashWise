61bc68d430156f5a377e6a57d2f46fdb0a0a5e31
Aslly Zuñiga <asllyz2610@gmail.com>
2026-03-17 23:55:28 -0500

    feat: add transaction modal trigger with open state

--- src/components/Transactions/transaction.tsx
+++ src/components/Transactions/transaction.tsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
-import { UiButton } from "../../webComponents";
+import { UiButton } from "@asllyzuniga/react-library";
 import TransactionForm from "./transactionForm";
+import "./Transaction.scss"; // Asegúrate de importar los estilos
 
 export default function Transaction() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <UiButton
-        className="new-transaction-button"
-        onClick={() => setIsOpen(true)}
-      >
-        + Nueva Transacción
-      </UiButton>
+      {/* Contenedor que empuja el botón a la derecha */}
+      <div className="transaction-header">
+        <UiButton
+          className="new-transaction-button react-transaction"
+          onClick={() => setIsOpen(true)}
+        >
+          + Nueva Transacción
+        </UiButton>
+      </div>
 
-      <TransactionForm isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      {/* Modal */}
+      <TransactionForm
+        externalOpen={isOpen}
+        onCloseExternal={() => setIsOpen(false)}
+      />
     </>
   );
 }
